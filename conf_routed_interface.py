4cdc200aa6552598833aa185f910f8dcb7685fd5
Vincenzo Vinciguerra <vvinciguerra6@gmail.com>
2025-08-13 14:17:25 +0200

    Add files via upload

---
+++ conf_routed_interface.py
@@ -0,0 +1,70 @@
+import os
+from netmiko import ConnectHandler
+
+SSH_CONFIG_PATH = os.path.expanduser('C:/Users/User/.ssh/config')
+
+def connect_to_device(device_ip):
+
+    device={
+        'device_type' : 'cisco_ios' ,
+        'ip' : device_ip ,
+        'username' : 'admin' ,
+        'password' : 'cisco' ,
+        'ssh_config_file': SSH_CONFIG_PATH
+    }
+
+    return device
+
+
+answer=True
+while(answer==True):
+    print('/****** ROUTED INTERFACE CONF **********/\n\n')
+    print("Enter DEVICE IP you want to connect:")
+    device_ip=input()
+    print('\n\n')
+
+    print("Enter INTERFACE to configure:")
+    interface=input()
+    print('\n\n')
+
+    print("Enter IP ADDRESS :")
+    ip_address=input()
+    print('\n\n')
+
+    print("Enter IP NETMASK :")
+    netmask=input()
+    print('\n\n')
+
+
+    device=connect_to_device(device_ip)
+    net_connect=ConnectHandler(**device)
+    list_of_commands=["interface "+interface,
+                      "no switchport",
+                      "ip address "+ip_address+" "+netmask,
+                      "no shutdown",
+                      ]
+    net_connect.send_config_set(list_of_commands)
+    net_connect.save_config()
+    print('\n\n')
+
+    print('/****** ROUTED INTERFACE CONF **********/\n\n')
+
+    output=net_connect.send_command("show running-config interface "+interface)
+    print(f'DEVICE IP: {device['ip']}\n\n{output}\n\n')
+
+    print('/*********************************************/\n\n')
+
+    error_typo=True
+    while(error_typo == True):
+        print('Would you connect to another device [Yes/No]:')
+        question=input()
+        print('\n')
+        if(question != 'Yes'):
+            if(question != 'No'):
+                print('Please , type either "No" or "Yes"\n')
+            else:
+                error_typo = False
+                answer = False
+        else:
+            error_typo = False
+
---
+++ show_ip_interface_brief.py
@@ -0,0 +1,49 @@
+import os
+from netmiko import ConnectHandler
+
+SSH_CONFIG_PATH = os.path.expanduser('C:/Users/User/.ssh/config')
+
+def connect_to_device(device_ip):
+
+    device={
+        'device_type' : 'cisco_ios' ,
+        'ip' : device_ip ,
+        'username' : 'admin' ,
+        'password' : 'cisco' ,
+        'ssh_config_file': SSH_CONFIG_PATH
+    }
+
+    return device
+
+
+answer = True
+while (answer==True):
+    print('/*********************************************/\n\n')
+    print('Please , enter device IP you want to inspect :')
+    device_ip=input()
+    print('\n\n')
+
+
+    device=connect_to_device(device_ip)
+    net_connect=ConnectHandler(**device)
+
+    print('/****** SHOW IP INTERFACE CONF **********/\n\n')
+    output=net_connect.send_command("show ip interface brief")
+    print(f'DEVICE IP: {device['ip']}\n\n{output}\n\n')
+
+    print('/*********************************************/\n\n')
+
+    error_typo=True
+    while(error_typo == True):
+        print('Would you connect to another device [Yes/No]:')
+        question=input()
+        print('\n')
+        if(question != 'Yes'):
+            if(question != 'No'):
+                print('Please , type either "No" or "Yes"\n')
+            else:
+                error_typo = False
+                answer = False
+        else:
+            error_typo = False
+
